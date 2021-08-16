6213a2a3cdc43283bb6903be94d2e911b4e6f4df
Topik Hidayat <aripj99@gmail.com>
2021-08-16 13:00:17 +0700

    komunikasi 2 arah, masih ada bug

--- comm_arduino/comm_arduino.ino
+++ comm_arduino/comm_arduino.ino
@@ -1,15 +1,36 @@
+  
 #include <SoftwareSerial.h>
+
 SoftwareSerial s(5,6);
- 
 String data;
+
 void setup() {
+// Open serial communications and wait for port to open:
 Serial.begin(115200);
 s.begin(115200);
-delay(2000);
+while (!Serial) {
+; // wait for serial port to connect. Needed for native USB port only
 }
- 
-void loop() {
-data =String("suhu = 10, lempap = 20");
-s.println(data);
-delay(1000);
+}
+
+void loop() { // run over and over
+  
+data =String("suhu = 10, lempap = 20 cok");
+
+kirimdata();
+//terimadata();
+}
+
+void kirimdata()
+{
+  s.println(data);
+  delay(1000);
+}
+
+
+void terimadata()
+{
+  if (Serial.available()) {
+  Serial.write(Serial.read());
+}
 }
--- comm_nodemcu/comm_nodemcu.ino
+++ comm_nodemcu/comm_nodemcu.ino
@@ -1,15 +1,38 @@
 #include <SoftwareSerial.h>
-SoftwareSerial s(D6,D5);
-void setup() {
-// Open serial communications and wait for port to open:
+String str;
+char dataterima[100];
+
+void setup(){
 Serial.begin(115200);
-while (!Serial) {
-; // wait for serial port to connect. Needed for native USB port only
+Serial1.begin(115200);
+delay(2000);
 }
+void loop()
+{
+str =String("coming from ESP8266: ");
+
+terimadata();
+//kirimdata();
+
+//delay(1000);
 }
 
-void loop() { // run over and over
-if (Serial.available()) {
-Serial.write(Serial.read());
+void kirimdata()
+{
+    Serial1.println(str);
+    delay(1000);
 }
+
+void terimadata()
+{
+    if (Serial.available()) {
+    Serial.write(Serial.read());
+
+//    sprintf(dataterima, "%d", Serial.read());
+        
+//    if(strstr(Serial.read(), "cok"))
+//    {
+//        Serial.println("found");
+//        }
+    }
 }
